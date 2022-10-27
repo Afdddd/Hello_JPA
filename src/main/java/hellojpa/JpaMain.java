9d900ab6c79c163849aa7f45083277a76acb2ef1
inyeop <kimdlsduq36@gmail.com>
2022-10-27 23:31:44 +0900

    영속성 컨텍스트 - 동일성 보장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,15 +15,12 @@ public class JpaMain {
         try {
             tx.begin(); // 트랜잭션 시작
 
-            Member member1 = new Member(1L,"hello");
-            em.persist(member1); // 1차캐시에 저장
-            Member findMember = em.find(Member.class,1L); // 1차캐시에서 조회
+            Member findMember1 = em.find(Member.class, 1L);
+            Member findMember2 = em.find(Member.class, 1L);
+
+            System.out.println("result = "+(findMember1 == findMember2));
 
             tx.commit(); // 커밋
-
-//            데이터베이스에서 조회
-//            Member findMember1 = em.find(Member.class,2L); DB에서 조회
-//            Member findMember2 = em.find(Member.class,2L); 1차캐시에서 조회
         } catch (Exception e) {
             tx.rollback(); // 실패하면 롤백
         }finally {

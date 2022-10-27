93445040a92de44a09a69ab5fb93039cc88a7846
inyeop <kimdlsduq36@gmail.com>
2022-10-27 23:37:28 +0900

    영속성 컨텍스트 - 쓰기 지연

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,9 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+/**
+ * 쓰기 지연
+ */
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -15,10 +18,14 @@ public class JpaMain {
         try {
             tx.begin(); // 트랜잭션 시작
 
-            Member findMember1 = em.find(Member.class, 1L);
-            Member findMember2 = em.find(Member.class, 1L);
+            Member member1 = new Member(10L, "A");
+            Member member2 = new Member(11L, "B");
 
-            System.out.println("result = "+(findMember1 == findMember2));
+            em.persist(member1);
+            System.out.println("======================");
+            em.persist(member2);
+            System.out.println("======================");
+            //persist() 할때마다 쿼리를 보내지 않고 SQL저장소에 모아서 한번에 보낸다.
 
             tx.commit(); // 커밋
         } catch (Exception e) {

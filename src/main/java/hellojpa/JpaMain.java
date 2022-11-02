14e9ea77169a39502081bb8db159972dc99a59f6
inyeop <kimdlsduq36@gmail.com>
2022-11-03 00:07:49 +0900

    양방향 연관관계 주의점 1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -7,7 +7,8 @@ import javax.persistence.Persistence;
 import java.util.List;
 
 /**
- * 양방향 매핑
+ * 양방향 매핑시 주의할점 1.
+ *  연관관계의 주인에 값을 입력하지 않음.
  */
 public class JpaMain {
     public static void main(String[] args) {
@@ -19,25 +20,20 @@ public class JpaMain {
         try {
             tx.begin(); // 트랜잭션 시작
 
-            // Team 저장
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
-
-            // Member 저장
+            // 저장
             Member member = new Member();
             member.setName("MemberA");
-            member.setTeam(team);
+            // member.setTeam(team);  연관관계의 주인에게 값을 입력해야한다.
             em.persist(member);
+
+            Team team = new Team();
+            team.setName("TeamA");
+            team.getMembers().add(member); // mappedBy 읽기전용이기 때문에 데이터베이스에 반영이 안된다.
+            em.persist(team);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
-            for (Member m : members) {
-                System.out.println("m ="+ m.getName());
-            }
 
             tx.commit(); // 커밋
         } catch (Exception e) {
